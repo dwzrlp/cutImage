a48da45f6a4b6db17e3d3efdb81ffed0d24c3186
dwzrlp <dwzrlp@163.com>
2018-03-23 16:25:57 +0100

    getImage

--- imageCut.py
+++ imageCut.py
@@ -1,6 +1,6 @@
 # coding = utf-8
-
 import os
+import time
 
 from PIL import Image
 
@@ -15,14 +15,11 @@ def imageCut(image):
     # 距离图片上边界距离y，
     # 距离图片左边界距离+裁剪框宽度x+w，
     # 距离图片上边界距离+裁剪框高度y+h）
-
     # 切割头部表示区域
     img2 = img.crop((0, 143, width, height))
-
-    # 截取图片文件名的序号
-    imgNumber = img.filename.split('-')[1]
-    # 重新命名文件名 wifi-加序号
-    img2FileName = 'wifi-' + imgNumber
+    # 获得今天的日期
+    today = time.strftime("%Y-%m-%d", time.localtime())
+    img2FileName = 'wifi_' + str(today + '.jpg')
     # 将文件另存为新文件
     img2.save(img2FileName)
     os.remove(image)
